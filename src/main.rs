cbe445cfc751cfe30ff070faa180b7090f1ba650
Ivan Shevyrev <ivanvs140@yandex.ru>
2021-01-30 20:00:48 +0300

    All revisited. Arrays added

--- src/main.rs
+++ src/main.rs
@@ -2,77 +2,36 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("\nI am Vega. A UAC's artificial intelligence. Let's play 'ROCK, PAPER, SCISSORS' game..\n");
-
+    println!("\nI am Vega. A UAC's artificial intelligence. Let's play 'ROCK, PAPER, SCISSORS' game..");
+    let game_list = ["", "Rock", "Paper", "Scissors"];
     loop {
-        let vega_choise = rand::thread_rng().gen_range(1,4);
-        println!("> Vega made his choice\n");
+        let vega_choice = rand::thread_rng().gen_range(1,4);
+        println!("\nVega made his choice.\n");
 
         println!("Enter your choise: 1 - ROCK, 2 - PAPER, and 3 for SCISSORS");
-        let mut user_choise = String::new();
+        let mut user_choice = String::new();
         io::stdin()
-            .read_line(&mut user_choise)
+            .read_line(&mut user_choice)
             .expect("Failed to read line");
 
-        let user_choise: u32 = match user_choise.trim().parse() {
+        let user_choice: u32 = match user_choice.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        if user_choise == 1 {
-            println!("\n> Your choise is ROCK\n");
-        }
-        else if user_choise == 2 {
-            println!("\n> Your choise is PAPER\n");
-        }
-        else {
-            println!("\n> Your choise is SCISSORS\n");
-        };
+        println!("\nVega chose {}", game_list[vega_choice as usize]); // for now
+        println!("User chose is {}", game_list[user_choice as usize]);
 
-        if vega_choise == 1 {
-            println!("> Vega chose ROCK\n");
-        }
-        else if vega_choise == 2 {
-            println!("> Vega chose PAPER\n")
-        }
-        else {
-            println!("> Vega chose SCISSORS\n")
-        };
-
-        if vega_choise == user_choise {
-            println!("\nround draw\n");
+        if vega_choice == user_choice {
+            println!("\nDraw.");
             continue
         }
+        else if vega_choice == 1 && user_choice == 3 || vega_choice == 2 && user_choice == 1 || vega_choice == 3 && user_choice == 2 {
+            println!("\nVega wins! {} beats {}\n", game_list[vega_choice as usize], game_list[user_choice as usize]);
+            break
+        }
         else {
-            if vega_choise == 1 {
-                if user_choise == 3 {
-                    println!("Vega wins! ROCK beats SCISSORS\n");
-                    break
-                }
-                else if user_choise == 2 {
-                    println!("User wins! PAPPER beats ROCK\n");
-                    break
-                }
-            }
-            if vega_choise == 2 {
-                if user_choise == 3 {
-                    println!("User wins! PAPPER beats ROCK\n");
-                    break
-                }
-                else if user_choise == 1 {
-                    println!("Vega wins! PAPPER beats ROCK\n");
-                    break
-                }
-            }
-            if vega_choise == 3 {
-                if user_choise == 2 {
-                    println!("Vega wins! SCISSORS beats PAPPER\n");
-                    break
-                }
-                else if user_choise == 1 {
-                    println!("User wins! ROCK beats SCISSORS\n");
-                    break
-                }
-            }
+            println!("\nUser wins! {} beats {}\n", game_list[user_choice as usize], game_list[vega_choice as usize]);
+            break
         }
     }
 }
