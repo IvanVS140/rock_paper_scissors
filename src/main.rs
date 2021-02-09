b5177bc81b6ffe0933c910aa86b95c57dc983a60
Ivan Shevyrev <ivanvs140@yandex.ru>
2021-02-09 21:58:40 +0300

    Array modifed, indexes changed, user_choice type changed to usize, draw option modifed, cosmetic fixes

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use rand::Rng;
 
 fn main() {
     println!("\nI am Vega. A UAC's artificial intelligence. Let's play 'ROCK, PAPER, SCISSORS' game..");
-    let game_list = ["", "Rock", "Paper", "Scissors"];
+    let game_list = ["Rock", "Paper", "Scissors"];
     loop {
-        let vega_choice = rand::thread_rng().gen_range(1,4);
+        let vega_choice: usize = rand::thread_rng().gen_range(0,3);
         println!("\nVega made his choice.\n");
 
         println!("Enter your choise: 1 - ROCK, 2 - PAPER, and 3 for SCISSORS");
@@ -14,23 +14,26 @@ fn main() {
             .read_line(&mut user_choice)
             .expect("Failed to read line");
 
-        let user_choice: u32 = match user_choice.trim().parse() {
-            Ok(num) => num,
+        let user_choice = match user_choice.trim().parse::<usize>() {
+            Ok(num) => num - 1,
             Err(_) => continue,
-        };
-        println!("\nVega chose {}", game_list[vega_choice as usize]); // for now
-        println!("User chose is {}", game_list[user_choice as usize]);
 
-        if vega_choice == user_choice {
+        };
+        println!("\nVega chose {}", game_list[vega_choice]);
+        println!("User chose is {}", game_list[user_choice]);
+
+        if (vega_choice + 1) % 3 == user_choice {
             println!("\nDraw.");
             continue
         }
-        else if vega_choice == 1 && user_choice == 3 || vega_choice == 2 && user_choice == 1 || vega_choice == 3 && user_choice == 2 {
-            println!("\nVega wins! {} beats {}\n", game_list[vega_choice as usize], game_list[user_choice as usize]);
+        else if vega_choice == 1 && user_choice == 3 ||
+                vega_choice == 2 && user_choice == 1 ||
+                vega_choice == 3 && user_choice == 2 {
+            println!("\nVega wins! {} beats {}\n", game_list[vega_choice], game_list[user_choice]);
             break
         }
         else {
-            println!("\nUser wins! {} beats {}\n", game_list[user_choice as usize], game_list[vega_choice as usize]);
+            println!("\nUser wins! {} beats {}\n", game_list[user_choice], game_list[vega_choice]);
             break
         }
     }
