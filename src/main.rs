e3b6bb3faaec4257d89f9a93934dff452a6b9711
Ivan Shevyrev <ivanvs140@yandex.ru>
2021-03-01 23:10:09 +0300

    winning func separated, tests added, minor fixes

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("\nI am Vega. A UAC's artificial intelligence. Let's play 'ROCK, PAPER, SCISSORS' game..");
+    let meet_vega: &str = "I am Vega. A UAC's artificial intelligence";
+    let game_invite: &str = "Let's play 'ROCK, PAPER, SCISSORS' game";
+
+    println!("\n{}. {}..", meet_vega, game_invite);
+
     let game_list = ["Rock", "Scissors", "Paper"];
+
     loop {
         let vega_choice: usize = rand::thread_rng().gen_range(0,3);
-        println!("\nVega made his choice.\n");
+        println!("\nVega made his choice. {}\n", vega_choice + 1);
 
         println!("Enter your choise: 1 - ROCK, 2 - SCISSORS, and 3 for PAPER");
         let mut user_choice = String::new();
@@ -28,17 +33,42 @@ fn main() {
 
         println!("\n{} vs {}", game_list[user_choice], game_list[vega_choice]);
 
-        if vega_choice == user_choice {
-            println!("\nDraw.");
-            continue
-        }
-        else if vega_choice == (user_choice + 2) % 3 {
-            println!("\nVega wins! {} beats {}\n", game_list[vega_choice], game_list[user_choice]);
-            break
-        }
-        else {
-            println!("\nUser wins! {} beats {}\n", game_list[user_choice], game_list[vega_choice]);
-            break
+        if who_wins(vega_choice, user_choice) != 0 {
+            break;
         }
     }
+}
+
+fn who_wins(x: usize, y: usize) -> u32 {
+    if x == y {
+        println!("\nDraw.");
+        return 0;
+    }
+    else if x == (y + 2) % 3 {
+        println!("Vega wins!");
+        return 1;
+    }
+    else {
+        println!("User wins!");
+        return 2;
+    }
+}
+
+#[test]
+fn round_draw() {
+    assert_eq!(who_wins(0, 0), 0);
+    assert_eq!(who_wins(1, 1), 0);
+    assert_eq!(who_wins(2, 2), 0);
+}
+#[test]
+fn vega_wins() {
+    assert_eq!(who_wins(0, 1), 1);
+    assert_eq!(who_wins(1, 2), 1);
+    assert_eq!(who_wins(2, 0), 1);
+}
+#[test]
+fn user_wins() {
+    assert_eq!(who_wins(1, 0), 2);
+    assert_eq!(who_wins(2, 1), 2);
+    assert_eq!(who_wins(0, 2), 2);
 }
