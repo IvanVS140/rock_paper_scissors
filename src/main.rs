1bf7983874b34f4ee495c7526c3ca168efbc0f73
Ivan Shevyrev <ivanvs140@yandex.ru>
2021-03-21 20:25:48 +0300

    test commit

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rand::Rng;
 use functions::who_wins;
 
 fn main() {
-    let meet_vega: &str = "I am Vega. A UAC's artificial intelligence";
+    let meet_vega: &str = "I am Vega. A UAC's artificial intelligence.";
     let game_invite: &str = "Let's play 'ROCK, SCISSORS, PAPER' game";
 
     println!("\n{}. {}..", meet_vega, game_invite);
